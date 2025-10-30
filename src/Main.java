a02d0f0dc7d3140e33ce1b82e793b3ad419ebd8c
berkee25 <berkeozdemir25@gmail.com>
2025-10-30 10:48:26 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+class exercise {
+    int max;
+    int top;
+    int [] arr;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    exercise (int max){
+        this.arr = new int[max];
+        this.max = max;
+        this.top = -1;
+    }
+    public void push(int val) {
+        if (top >= max-1) {
+            System.out.println("stacks overflow");
+        }else {
+            arr[++top] = val;
+            System.out.println(val + " pushed into stack");
+        }
+    }
+    public int pop() {
+        if (top < 0) {
+            System.out.println("stacks overflow");
+            return -1;
+        }else {
+            int val = arr[top--];
+            System.out.println(val + " popped from stack");
+            return val;
+        }
+    }
+    public int peek() {
+        if (top < 0) {
+            System.out.println("Stacks overflow");
+            return -1;
+        }else {
+            return arr[top];
+        }
+    }
+    public static void main(String[] args) {
+        exercise s = new exercise(5);
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println("Top element: " + s.peek());
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop(); // boşken pop
+    }
 }
